97e56b1ffd91c264fb3b103b9649f307984436a1
Rajasekaran Kalidoss <rkalidos@codeaurora.org>
2019-02-25 12:05:15 +0530

    cnss2: Add support for QCN7605 ver 2 usb device support
    New PID has been introduced for Version 2.0 of QCN7605
    USB composite device. PID should be updated in cnss2
    driver to support this device.
    
    Change-Id: I12b944b15021f317b24a34b0801a4d600133eee3
    Signed-off-by: Rajasekaran Kalidoss <rkalidos@codeaurora.org>

--- drivers/net/wireless/cnss2/main.c
+++ drivers/net/wireless/cnss2/main.c
@@ -1392,6 +1392,8 @@ int cnss_register_subsys(struct cnss_plat_data *plat_priv)
 	case QCN7605_DEVICE_ID:
 	case QCN7605_STANDALONE_DEVICE_ID:
 	case QCN7605_COMPOSITE_DEVICE_ID:
+	case QCN7605_VER20_STANDALONE_DEVICE_ID:
+	case QCN7605_VER20_COMPOSITE_DEVICE_ID:
 		subsys_info->subsys_desc.name = "QCN7605";
 		break;
 	default:
@@ -1614,6 +1616,8 @@ int cnss_register_ramdump(struct cnss_plat_data *plat_priv)
 		break;
 	case QCN7605_COMPOSITE_DEVICE_ID:
 	case QCN7605_STANDALONE_DEVICE_ID:
+	case QCN7605_VER20_STANDALONE_DEVICE_ID:
+	case QCN7605_VER20_COMPOSITE_DEVICE_ID:
 		break;
 
 	default:
@@ -1636,6 +1640,8 @@ void cnss_unregister_ramdump(struct cnss_plat_data *plat_priv)
 		break;
 	case QCN7605_COMPOSITE_DEVICE_ID:
 	case QCN7605_STANDALONE_DEVICE_ID:
+	case QCN7605_VER20_STANDALONE_DEVICE_ID:
+	case QCN7605_VER20_COMPOSITE_DEVICE_ID:
 		break;
 	default:
 		cnss_pr_err("Unknown device ID: 0x%lx\n", plat_priv->device_id);
